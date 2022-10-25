f00058641d5f1eef7053dada5790120b13282777
ProfessionalWeslley <102042491+ProfessionalWeslley@users.noreply.github.com>
2022-10-25 08:49:45 -0300

    Add files via upload

---
+++ INICIO.js
@@ -0,0 +1,5 @@
+//window.alert('bem vindo')
+
+//window.prompt('qual seu nome?')
+
+//$nome = prompt
---
+++ home.js
@@ -0,0 +1,5 @@
+/*function mUp(obj) {
+    obj.style.backgroundColor="#1cb330".width= "65%". height= "150px";
+    
+    
+  }*/
